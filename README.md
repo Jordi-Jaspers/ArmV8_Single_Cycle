27ff1ddd5704ac42365e9f32f1ff9f16b169642e
Jordi-Jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2018-12-05 14:04:27 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ArmV8_Single_Cycle
-An Arm V8 Single Cycle processor made with the instructions of the ARM-Edition book written by David A. Patterson &amp; John L. Hennesey
+#Author:
+Jordi Jaspers
+
+#Date of initial commit:
+05/12/2018
+
+# ArmV8_Single_Cycle [Biography]:
+An Arm V8 Single Cycle processor made with the instructions of the ARM-Edition book written by David A. Patterson & John L. Hennesey
+
+DISCLAIMER:
+All the code that is copied or used from other sources will be mentioned in the reference section of this file.
+
+#TODO list:
+The single cycle processor is made out of 9 modules and ofcourse a testfile.
+
+1.  RegisterFile    	-->   done!
+2.  ProgramCounter      -->
+3.  SignExtend          -->
+4.  ALU                 -->
+5.  ALUControl          -->
+6.  DataMemory          -->
+7.  InstructionMem      -->
+8.  SingleCycle         -->
+9.  SingleCycleControl  -->
+10. Test file?
+
+
+
+#References:
+
+
+
+
---
+++ RegisterFile.v
@@ -0,0 +1,34 @@
+module RegisterFile(R1, R2, WReg, Data, WE, Out1, Out2, Clk);
+	input Clk;						//Clock signaal
+	
+	input [4:0] R1, R2;				//5-bits -> 32 plekker in een register
+	input [4:0]	WReg				//Bepaalt welke register naar to schrijven
+	input [64:0] Data				//DAta die wegeschreven moet worden
+	input WE;						//Write-enable
+	
+	output [63:0] Out1, Out2;		//2 - 64bit data uitvoer
+	reg [63:0] registers [31:0]; 	//32 registers van 64-bit
+
+	//Assign data outputs naar de 65 bit registers
+	assign #2 Out1 = registers[R1];
+	assign #2 Out2 = registers[R2];
+
+	initial
+	begin
+		registers[31] <= 64'b0; 	//64 bit binary literals (64'b0)
+	end
+    
+	//Door een always the doen maakt verilog automatisch een sensitivity-list
+	//Als WE is HIGH en WREG is 5bit dan mag schrijf de data naar WReg register
+	always @ (negedge Clk)
+	begin
+		if(WE && WReg != 5'b11111)
+		registers[WReg] <= #3 Data;
+	end
+	
+
+	always @ (*)
+	begin
+		registers[31] <= {64{1'b0}}; //set register 31 terug naar 0
+	end
+endmodule
